3ee85f3d8e588755ae54e5c3c120c4fe2490aa3c
bkpark <bokyoungp@naver.com>
2023-09-26 17:03:46 +0900

    react day2 v4.0

--- src/sync-ex.js
+++ src/sync-ex.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 
+// 동기적 처리
 console.log("1) file read start");
 let data = fs.readFileSync('sync.txt', {encoding:'utf8'});
 console.log("2) file read end");
 console.log("3) " + data);
 
+// 비동기적 처리 
 console.log("1) file read start");
 fs.readFile('sync.txt', {encoding:'utf8'}, (err, data) => {
     if (err) throw err;
-    console.log("2)" + data);
+    console.log("2) " + data);
   });
 console.log("3) file read end");
